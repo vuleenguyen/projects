d29e76d2cdf583350173227788053e84343645b0
vuleenguyen <vuleenguyen.92@gmail.com>
2017-02-24 16:33:01 -0800

    commit for Unit7

---
+++ SpringUnit7/src/main/java/com/edu/mum/SpringUnit7Application.java
@@ -0,0 +1,12 @@
+package com.edu.mum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUnit7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUnit7Application.class, args);
+	}
+}
